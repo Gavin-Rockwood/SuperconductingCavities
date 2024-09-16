ca16b081f50a137143298555efd91e66d1cca870
Gavin-Rockwood <gavin.rockwood@gmail.com>
2024-09-15 23:27:41 -0400

    still working on the quantum toolbox. need to figure out how to calculate propagators

--- src_qt/Utils/TimeEvolutionUtils.jl
+++ src_qt/Utils/TimeEvolutionUtils.jl
@@ -1,13 +1,10 @@
+import QuantumToolbox as qt
 include("ExtraStuff/Envelopes.jl")
 
 export Get_Drive_Hamiltonian, Get_Drive_Hamiltonian_With_Envelope, f_for_schroedinger_dynamic, f_for_schroedinger_dynamic, f_for_master_dynamic
 
-function Get_Drive_Hamiltonian(model, op, ν, ε)
-    return Get_Drive_Hamiltonian_With_Envelope(model, op, ν, ε, Square_Envelope)
-end
-
-function Get_Drive_Hamiltonian_With_Envelope(model, op, ν, ε, envelope)
-    return t->2*pi*(ε*envelope(t)*sin(2π*ν*t)*op)
+function Get_Drive_Coef(ν, ε, envelope = Square_Envelope)
+    return t->2*pi*(ε*envelope(t)*sin(2π*ν*t))
 end
 
 function Get_Envelope(envelope_name, envelope_kwargs)
@@ -19,4 +16,18 @@ function Get_Envelope(envelope_name, envelope_kwargs)
         return Envelope_Dict[envelope_name](t; envelope_kwargs_sym...)
     end
     return envelope
+end
+
+function Get_Ĥₜ(op::qt.QuantumObject, drive_coef::Union{Nothing, Function}; TDOS = true, params = nothing, init_time = 0.0)
+    drive_coef = (drive_coef isa Nothing) ? (t)->1 : drive_coef
+
+    if TDOS
+        return qt.TimeDependentOperatorSum([cf], [op], params = params, init_time = init_time)
+    else
+        return t -> drive_coef(t)*op
+    end
+end
+
+function Get_Lₜ(op, drive_coef; params = nothing, init_time = 0.0)
+    return qt.TimeDependentOperatorSum([drive_coef], [qt.liouvillian(op)], params = params, init_time = init_time)
 end
