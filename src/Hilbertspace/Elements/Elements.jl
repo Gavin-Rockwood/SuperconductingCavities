f818b9a9735380dc842b27bf9e33319a100da614
Gavin-Rockwood <gavin.rockwood@gmail.com>
2024-10-06 20:17:02 -0400

    working on snails!

--- src/Hilbertspace/Elements/Elements.jl
+++ src/Hilbertspace/Elements/Elements.jl
@@ -1,8 +1,12 @@
 module Elements
+    using ..Utils
     include("Resonators.jl")
     using .Resonators
 
     include("Transmons.jl")
     using .Transmons
+
+    include("SNAIL.jl")
+    using .SNAILs
 
 end
