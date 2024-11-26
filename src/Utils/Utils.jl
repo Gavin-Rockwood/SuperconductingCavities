b294d0c934d1e52489e6a2e7b54b55ff434f494b
Gavin <gavin.rockwood@gmail.com>
2024-11-26 02:50:33 -0500

    fixed stackoverflow error in Qobs_list_to_DS

--- src/Utils/Utils.jl
+++ src/Utils/Utils.jl
@@ -24,18 +24,22 @@ module Utils
     end
 
     function Qobj_List_To_DS(QOL; cube_name = :Default, cube_properties = Dict{Any, Any}(), ds_properties = Dict{Any, Any}(), step_name = :Step)
-        step1 = collect.(QOL)
-        
+        step1 = collect.(qt.sparse_to_dense.(QOL))
         
         if length(size(step1[1])) == 1
-            step2 = collect.(reim.(step1));
-            step3 = permutedims(cat(reduce.(hcat, step2)..., dims = 3), (3,1,2));
+            step2 = reduce.(hcat, collect.(reim.(step1)))
+            a = length(step2[1][:,1])
+            b = length(step2[1][1,:])
+            c = length(step2)
+            step3 = permutedims(reshape(mapreduce(xs -> vec(reduce(hcat, xs)), hcat, step2), a,b,c), (3,1,2));
             dim = size(step3)
             axlist = (Dim{step_name}(1:dim[1]),  Dim{:i}(1:dim[2]), Dim{:P}(["Re", "Im"]))
         elseif length(size(step1[1])) > 1
-            step2 = collect.(reim(step1))
-            step3 = permutedims(cat(reduce.((x,y) -> cat(x, y, dims = 3), step2)..., dims = 4), (3, 1,2,4));
-            
+            step2 = reduce.(hcat, collect.(reim.(step1)))
+            a = length(step2[1][:,1])
+            b = length(step2[1][1,:])
+            c = length(step2)
+            step3 = permutedims(reshape(mapreduce(xs -> vec(reduce(hcat, xs)), hcat, step2), a,a,b,c), (3,1,2.4));
             dim = size(step3)
             
             axlist = (Dim{step_name}(1:dim[1]),  Dim{:i}(1:dim[2]), Dim{:j}(1:dim[3]), Dim{:P}(["Re", "Im"]))
